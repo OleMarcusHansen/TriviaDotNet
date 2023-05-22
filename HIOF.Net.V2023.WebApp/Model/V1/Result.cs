4ad7777fa2bd93f7c7b9b1b088f236151d9a5a60
Fillip Novak Grunnsund <fillnova@hotmail.com>
2023-05-22 11:13:42 +0200

    feat: add fucntionality for writing score to database

---
+++ HIOF.Net.V2023.WebApp/Model/V1/Result.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HIOF.Net.V2023.DatabaseService.Model.V1
+{
+    public class Result<T>
+    {
+        public Result(T value)
+        {
+            Value = value;
+        }
+
+        public List<string> Errors { get; set; } = new List<string>();
+
+        [JsonIgnore]
+        public bool HasErrors => Errors.Any();
+
+        public T Value { get; set; }
+    }
+}
