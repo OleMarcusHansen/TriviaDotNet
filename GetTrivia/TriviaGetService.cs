8ebd61dbe5b3bf258006365d2b1e526238e59a66
Ole Marcus Hansen <omhanse@hiof.no>
2023-05-23 11:00:01 +0200

    Change console service from rest to grpc to retrieve quiz

--- GetTrivia/TriviaGetService.cs
+++ GetTrivia/TriviaGetService.cs
@@ -1,16 +1,48 @@
-﻿using Grpc.Core;
+﻿using GetTrivia.Controllers.V1;
+using GetTrivia.GetTriviaService.Model.V1;
+using Grpc.Core;
+using System.Text.Json;
 
 namespace HIOF.Net.V2023.GetTriviaService
 {
     public class TriviaGetService : TriviaService.TriviaServiceBase
     {
-        public override Task<GetTriviaResponse> GetTrivia(GetTriviaRequest request, ServerCallContext context)
-        {
-            var response = new GetTriviaResponse();
-            response.JsonData = "test grpc";
+        private readonly ILogger<TriviaGetService> _logger;
 
-            // Return the response to the caller
-            return Task.FromResult(response);
+        public TriviaGetService(ILogger<TriviaGetService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async override Task<GetTriviaResponse> GetTrivia(GetTriviaRequest request, ServerCallContext context)
+        {
+            using var client = new HttpClient();
+
+            //Er kategori og difficulty er valgfri, kan vi bruke ternery
+            _logger.LogInformation("Getting data from API");
+            var jsonFileUrl = $"https://the-trivia-api.com/api/questions?categories={request.Category}&limit={request.NumberOfQuestions}&difficulty={request.Difficulty}";
+            var questions = await client.GetFromJsonAsync<Quest[]>(jsonFileUrl);
+
+            if (request.Category == null)
+            {
+                _logger.LogError("Invalid category name given");
+            }
+
+            if (request.NumberOfQuestions >= 0)
+            {
+                _logger.LogError("Invalid Number of questions");
+            }
+
+            if (request.Difficulty == null)
+            {
+                _logger.LogError("Invalid difficulity given");
+
+            }
+
+            var response = new GetTriviaResponse();
+            response.JsonData = JsonSerializer.Serialize(questions);
+
+            return response;
         }
     }
 }
